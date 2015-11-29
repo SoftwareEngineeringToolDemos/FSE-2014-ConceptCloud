d2e5031ba47b01cccd33655cff2e7c4ccbe3ab95
gauri145 <gaurinaik.145@gmail.com>
2015-11-28 22:14:47 -0500

    Minor edit

--- build-vm/vm-contents/start-tool.sh
+++ build-vm/vm-contents/start-tool.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /home/vagrant/Desktop/conceptcloud/bin
+cd /home/vagrant/Desktop/conceptcloud
 rm RUNNING_PID
+cd /home/vagrant/Desktop/conceptcloud/bin
 ./conceptcloud &
 google-chrome "http://localhost:9000"
